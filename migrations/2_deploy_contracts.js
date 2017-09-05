61b4fda752817fb674911ab03187ab90840c47a4
Karel Striegel <karel.striegel@gmail.com>
2017-09-05 14:51:46 +0200

    Upgrade to Crowdsale contract, transfer still buggy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,20 @@
 var SafeMath = artifacts.require("./math/SafeMath.sol");
+var SafeMathLib = artifacts.require("./math/SafeMathLib.sol");
 //var ERC20 = artifacts.require("./ERC20.sol");
-var StandardToken = artifacts.require("./StandardToken.sol");
+var CrowdsaleToken = artifacts.require("./CrowdsaleToken.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(SafeMath);
-  //deployer.deploy(ERC20);
-  deployer.deploy(StandardToken);
+  deployer.deploy(SafeMathLib);
+  deployer.link(SafeMathLib, CrowdsaleToken)
+  deployer.deploy(CrowdsaleToken,
+    "FundRequest",
+    "FND",
+    666000000000000000000,
+    18,
+    true
+  );
 };
+
+//function CrowdsaleToken(string _name, string _symbol, uint _initialSupply, uint _decimals, bool _mintable)
+    //UpgradeableToken(msg.sender) {
