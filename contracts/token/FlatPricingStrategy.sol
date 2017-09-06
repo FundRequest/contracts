bffe3ffda5d6e27083106e3a762bbcb4ebc4c779
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-06 10:07:40 +0200

    token contracts added

---
+++ contracts/token/FlatPricingStrategy.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.6;
+
+import "./PricingStrategy.sol";
+import "../math/SafeMathLib.sol";
+
+/**
+ * Fixed crowdsale pricing - everybody gets the same price.
+ */
+contract FlatPricing is PricingStrategy {
+
+  using SafeMathLib for uint;
+
+  /* How many weis one token costs */
+  uint public oneTokenInWei;
+
+  function FlatPricing(uint _oneTokenInWei) {
+    oneTokenInWei = _oneTokenInWei;
+  }
+
+  /**
+   * Calculate the current price for buy in amount.
+   *
+   * @param  {uint amount} Buy-in value in wei.
+   */
+  function calculatePrice(uint value, uint weiRaised, uint tokensSold, address msgSender, uint decimals) public constant returns (uint) {
+    uint multiplier = 10 ** decimals;
+    return value.times(multiplier) / oneTokenInWei;
+  }
+
+}
---
+++ contracts/token/PricingStrategy.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.6;
+
+/**
+ * Interface for defining crowdsale pricing.
+ */
+contract PricingStrategy {
+
+  /** Interface declaration. */
+  function isPricingStrategy() public constant returns (bool) {
+    return true;
+  }
+
+  /** Self check if all references are correctly set.
+   *
+   * Checks that pricing strategy matches crowdsale parameters.
+   */
+  function isSane(address crowdsale) public constant returns (bool) {
+    return true;
+  }
+
+  /**
+   * When somebody tries to buy tokens for X eth, calculate how many tokens they get.
+   *
+   *
+   * @param value - What is the value of the transaction send in as wei
+   * @param tokensSold - how much tokens have been sold this far
+   * @param weiRaised - how much money has been raised this far
+   * @param msgSender - who is the investor of this transaction
+   * @param decimals - how many decimal units the token has
+   * @return Amount of tokens the investor receives
+   */
+  function calculatePrice(uint value, uint weiRaised, uint tokensSold, address msgSender, uint decimals) public constant returns (uint tokenAmount);
+}
