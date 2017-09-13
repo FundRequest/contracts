bd011d8e65fe5b76197f78cfe5a318f5212d7a39
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-13 16:54:20 +0200

    Migrating MintableFunctionality to es6

--- test/MintableFunctionality.js
+++ test/MintableFunctionality.js
@@ -1,74 +1,48 @@
-var FND = artifacts.require('./token/FundRequestToken.sol');
-var expect = require('chai').expect;
-var log = console.log;
+const FND = artifacts.require('./token/FundRequestToken.sol');
+const expect = require('chai').expect;
+const log = console.log;
 
 contract('FundRequestToken', function (accounts) {
 
-    var fnd;
-    var owner = accounts[0];
+    let fnd;
+    let owner = accounts[0];
 
-    beforeEach(function (done) {
-        FND.new("FundRequest",
-                "FND",
-                666000000000000000000,
-                18,
-                true).then(function (instance) {
-                fnd = instance;
+    beforeEach(async function () {
+        fnd = await FND.new("FundRequest", "FND", 666000000000000000000, 18, true);
+        await fnd.setMintAgent(accounts[0], true, {
+            from: accounts[0]
+        });
+    });
+
+    it('should not be possible to be mintagent if not allowed to be', async function () {
+        let isMintAgent = await fnd.mintAgents.call(accounts[1]);
+        expect(isMintAgent).to.be.false;
+    });
+
+    it('should be possible to set the mint agent', async function () {
+        let isMintAgent = await fnd.mintAgents.call(accounts[0]);
+        expect(isMintAgent).to.be.true;
+    });
+
+    it('should be possible to mint as a mintAgent', async function () {
+        await fnd.mint(accounts[1], 20)
+        let balance = await fnd.balanceOf.call(accounts[1]);
+        expect(balance.toNumber()).to.equal(20);
+    });
+
+    it('should not be possible to mint as a non-mintAgent', async function () {
+        try {
+            await fnd.mint(accounts[2], 20, {
+                from: accounts[1]
             })
-            .then(done);
-    });
-
-    it('contract should be set', function () {
-        expect(fnd).to.not.be.null;
-    });
-
-
-    it('should not be possible to be mintagent if not allowed to be', function () {
-        return fnd.setMintAgent(accounts[0], true, {
-            from: accounts[0]
-        }).then(function (res) {
-            return fnd.mintAgents.call(accounts[1]);
-        }).then(function (res) {
-            console.log("should be false: " + res);
-            expect(res).to.be.false;
-        });
-    });
-
-    it('should be possible to set the mint agent', function () {
-        return fnd.setMintAgent(accounts[0], true, {
-            from: accounts[0]
-        }).then(function (res) {
-            return fnd.mintAgents.call(accounts[0]);
-        }).then(function (res) {
-            console.log("should be true: " + res);
-            expect(res).to.be.true;
-        });
-    });
-
-    it('should be possible to mint as a mintAgent', function () {
-        return fnd.setMintAgent(accounts[0], true, {
-            from: accounts[0]
-        }).then(function (res) {
-            return fnd.mint(accounts[1], 20);
-        }).then(function (res) {
-            return fnd.balanceOf.call(accounts[1]);
-        }).then(function (res) {
-            expect(res.toNumber()).to.equal(20);
-        });
-    });
-
-    it('should not be possible to mint as a non-mintAgent', function () {
-        return fnd.setMintAgent(accounts[0], true, {
-                from: accounts[0]
-            }).then(function (res) {
-                return fnd.mint(accounts[2], 20, {
-                    from: accounts[1]
-                });
-            }).catch(log)
-            .then(function (res) {
-                return fnd.balanceOf.call(accounts[2]);
-            }).then(function (res) {
-                expect(res.toNumber()).to.equal(0);
-            });
+            assert.fail('fnds should never have been minted');
+        } catch (error) {
+            assert(
+                error.message.indexOf('invalid opcode') >= 0,
+                'releaseTokenTransfer should throw an opCode exception.'
+            );
+        }
+        let balance = await fnd.balanceOf.call(accounts[2]);
+        expect(balance.toNumber()).to.equal(0);
     });
 });
