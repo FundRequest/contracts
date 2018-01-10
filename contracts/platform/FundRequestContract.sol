e85a31e5c641fd1fded3fbba98450b40617e6238
davyvanroy <davy.van.roy@gmail.com>
2018-01-10 14:25:14 +0100

    some refactoring

--- contracts/platform/FundRequestContract.sol
+++ contracts/platform/FundRequestContract.sol
@@ -138,79 +138,20 @@ contract FundRequestContract is Owned, ApproveAndCallFallBack {
   }
 
   function validClaim(bytes32 platform, bytes32 platformId, string solver, address solverAddress, bytes32 r, bytes32 s, uint8 v) internal view returns (bool) {
-    var claimMsg = strConcat(
-    strConcat(bytes32ToString(platform), "_"),
-    strConcat(bytes32ToString(platformId), "_"),
-    strConcat(solver, "_"),
-    strConcat("0x", toAsciiString(solverAddress))
-    );
-    var h = sha3(claimMsg);
+    var h = sha3(createClaimMsg(platform, platformId, solver, solverAddress));
     address signerAddress = ecrecover(h, v, r, s);
     return claimSignerAddress == signerAddress;
   }
 
-
-  function toAsciiString(address x) internal pure returns (string) {
-    bytes memory s = new bytes(40);
-    for (uint i = 0; i < 20; i++) {
-      byte b = byte(uint8(uint(x) / (2 ** (8 * (19 - i)))));
-      byte hi = byte(uint8(b) / 16);
-      byte lo = byte(uint8(b) - 16 * uint8(hi));
-      s[2 * i] = charToByte(hi);
-      s[2 * i + 1] = charToByte(lo);
-    }
-    return string(s);
+  function createClaimMsg(bytes32 platform, bytes32 platformId, string solver, address solverAddress) internal pure returns (string) {
+    return strings.bytes32ToString(platform)
+    .strConcat(prependUnderscore(strings.bytes32ToString(platformId)))
+    .strConcat(prependUnderscore(solver))
+    .strConcat(prependUnderscore(strings.addressToString(solverAddress)));
   }
 
-  function charToByte(byte b) internal pure returns (byte c) {
-    if (b < 10) return byte(uint8(b) + 0x30);
-    else return byte(uint8(b) + 0x57);
-  }
-
-  function bytes32ToString(bytes32 x) internal pure returns (string) {
-    bytes memory bytesString = new bytes(32);
-    uint charCount = 0;
-    for (uint j = 0; j < 32; j++) {
-      byte ch = byte(bytes32(uint(x) * 2 ** (8 * j)));
-      if (ch != 0) {
-        bytesString[charCount] = ch;
-        charCount++;
-      }
-    }
-    bytes memory bytesStringTrimmed = new bytes(charCount);
-    for (j = 0; j < charCount; j++) {
-      bytesStringTrimmed[j] = bytesString[j];
-    }
-    return string(bytesStringTrimmed);
-  }
-
-  function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
-    bytes memory _ba = bytes(_a);
-    bytes memory _bb = bytes(_b);
-    bytes memory _bc = bytes(_c);
-    bytes memory _bd = bytes(_d);
-    bytes memory _be = bytes(_e);
-    string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-    bytes memory babcde = bytes(abcde);
-    uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-    for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-    for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-    for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-    for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-    return string(babcde);
-  }
-
-  function strConcat(string _a, string _b, string _c, string _d) internal pure returns (string) {
-    return strConcat(_a, _b, _c, _d, "");
-  }
-
-  function strConcat(string _a, string _b, string _c) internal pure returns (string) {
-    return strConcat(_a, _b, _c, "", "");
-  }
-
-  function strConcat(string _a, string _b) internal pure returns (string) {
-    return strConcat(_a, _b, "", "", "");
+  function prependUnderscore(string str) internal pure returns (string) {
+    return "_".strConcat(str);
   }
 
 }
