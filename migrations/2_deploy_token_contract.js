253d12da4d95df14dadad767ca26a133abc3aacf
Quinten De Swaef <quintendeswaef@gmail.com>
2018-02-12 16:04:00 +0100

    intermediate to deploy the main platform on mainnet

--- migrations/2_deploy_token_contract.js
+++ migrations/2_deploy_token_contract.js
@@ -1,16 +1,6 @@
 const MiniMeTokenFactory = artifacts.require("./factory/MiniMeTokenFactory.sol");
 const FundRequestToken = artifacts.require("./token/FundRequestToken.sol");
 
-module.exports = function (deployer) {
-	deployer.deploy(MiniMeTokenFactory).then(function () {
-		return deployer.deploy(FundRequestToken,
-			MiniMeTokenFactory.address,
-			0x0,
-			0,
-			"FundRequest",
-			18,
-			"FND",
-			false
-		);
-	});
+module.exports = function (deployer, network, accounts) {
+	//already deployed
 };
