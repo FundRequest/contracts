5fcad431058a11ffeb9c39a68b1730c1a24a8c1f
Quinten De Swaef <quintendeswaef@gmail.com>
2018-02-12 16:16:24 +0100

    updated scripts

--- scripts/vw-allocation.js
+++ scripts/vw-allocation.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const Web3 = require('web3');
 
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('https://ropsten.fundrequest.io'));
+web3.setProvider(new web3.providers.HttpProvider('https://mainnet.fundrequest.io'));
 web3.eth.getTransactionReceiptMined = require("./getTransactionReceiptMined.js");
 
 const p_key = process.env.PRIVATE_KEY;
@@ -26,13 +26,13 @@ const doThings = function (address, amount, _callback) {
 	} else {
 		let payloadData = solidityFunction.toPayload([address, amount]).data;
 
-		web3.eth.getTransactionCount('0xc31eb6e317054a79bb5e442d686cb9b225670c1d', function (_, nonce) {
+		web3.eth.getTransactionCount('0x0020d8f4052358ee01ef8d4164e5c97c95744235', function (_, nonce) {
 			web3.eth.sendRawTransaction(sign({
-				to: '0x93040a4f0fff7f975615ad69592631d207406d84',
+				to: '0xbcc546eb5a290977180f85cafaa712019893729c',
 				value: 0,
 				gas: 500000,
 				data: payloadData,
-				gasPrice: 20000000000,
+				gasPrice: 8000000000,
 				nonce: nonce
 			}, p_key), function (_, txHash) {
 				if (_) {
@@ -57,6 +57,9 @@ let iterate = function (lines) {
 	if (allocation.length > 5) {
 		let address = vw_address;
 		let tokensVested = allocation[5];
+
+		console.log('allocating tokens for ' + address + ' -> ' + tokensVested * Math.pow(10, 18));
+
 
 		doThings(address, tokensVested * Math.pow(10, 18), function (_callback) {
 			console.log("Done for address: " + address);
