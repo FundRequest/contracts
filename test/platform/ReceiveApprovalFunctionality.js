841701dccf35a346ae66dd2b631fcb80855fcc5a
Quinten De Swaef <quintendeswaef@gmail.com>
2018-03-22 15:52:09 +0100

    Finishing up preconfitions

--- test/platform/ReceiveApprovalFunctionality.js
+++ test/platform/ReceiveApprovalFunctionality.js
@@ -31,20 +31,6 @@ contract('ReceiveApprovalFunctionality', function (accounts) {
 		await claimRepository.updateCaller(frc.address, true, {from: owner});
 	});
 
-	it('should not be able to receive a different token to be approved by the fndContract', async function () {
-		try {
-			let fnd2 = await FND.new(tokenFactory.address, 0x0, 0, "FundRequest2", 18, "FND", true);
-			await fnd.changeController(owner);
-			await fnd2.generateTokens(owner, 666000000000000000000);
-
-			await fnd2.approveAndCall(frc.address, tokens(1), web3.fromAscii("github|1|https://github.com"));
-
-			assert.fail('should have failed');
-		} catch (error) {
-			assertInvalidOpCode(error);
-		}
-	});
-
 	it('should be possible to come in with approveAndCall', async function () {
 		let amount = tokens(1);
 		let platform = "github";
@@ -52,9 +38,9 @@ contract('ReceiveApprovalFunctionality', function (accounts) {
 
 		await fnd.approveAndCall(frc.address, amount, web3.fromAscii(platform + "|AAC|" + platformId), {from: owner});
 
-		let bal = await fundRepository.balance.call(web3.fromAscii(platform), platformId);
+		let bal = await fundRepository.balance.call(web3.fromAscii(platform), platformId, fnd.address);
 		expect(bal.toNumber()).to.equal(amount);
-		let fundInfo = await fundRepository.getFundInfo.call(web3.fromAscii(platform), platformId, owner);
+		let fundInfo = await fundRepository.getFundInfo.call(web3.fromAscii(platform), platformId, owner, fnd.address);
 		expect(fundInfo[0].toNumber()).to.equal(1);
 		expect(fundInfo[1].toNumber()).to.equal(amount);
 		expect(fundInfo[2].toNumber()).to.equal(amount);
