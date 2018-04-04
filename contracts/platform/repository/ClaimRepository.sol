c3bab64032d367c8e6b612a7e49b2ba0b91aa0c0
davyvanroy <davy.van.roy@gmail.com>
2018-04-04 13:39:53 +0200

    comment mappings add require when claiming to avoid misuse

--- contracts/platform/repository/ClaimRepository.sol
+++ contracts/platform/repository/ClaimRepository.sol
@@ -6,8 +6,10 @@ import "../../math/SafeMath.sol";
 contract ClaimRepository is Owned {
     using SafeMath for uint256;
 
+    //platform -> platformId => _claim
     mapping(bytes32 => mapping(string => Claim)) claims;
 
+    //sender => _allowed
     mapping(address => bool) public callers;
 
     uint256 public totalClaims;
@@ -32,6 +34,7 @@ contract ClaimRepository is Owned {
 
     function addClaim(address _solverAddress, bytes32 _platform, string _platformId, string _solver, address _token, uint256 _requestBalance) public onlyCaller returns (bool) {
         if (claims[_platform][_platformId].solverAddress != address(0)) {
+            require(claims[_platform][_platformId].solverAddress == _solverAddress);
             claims[_platform][_platformId].tokens.push(_token);
             claims[_platform][_platformId].amountPerTokens[_token] = _requestBalance;
         } else {
