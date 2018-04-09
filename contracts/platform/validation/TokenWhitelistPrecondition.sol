1f9e497d1d35193a4e82f7208d963a0997e04850
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-09 23:22:52 +0200

    fix major bug with preconditions

--- contracts/platform/validation/TokenWhitelistPrecondition.sol
+++ contracts/platform/validation/TokenWhitelistPrecondition.sol
@@ -1,12 +1,16 @@
 pragma solidity 0.4.21;
 
 import "./Precondition.sol";
+import "../../utils/strings.sol";
 
 
 contract TokenWhitelistPrecondition is Precondition {
 
+    using strings for *;
+
     event Allowed(address indexed token, bool allowed);
     event Allowed(address indexed token, bool allowed, bytes32 platform, string platformId);
+
 
     //platform -> platformId -> token => _allowed
     mapping(bytes32 => mapping(string => mapping(address => bool))) tokenWhitelist;
@@ -19,7 +23,7 @@ contract TokenWhitelistPrecondition is Precondition {
     }
 
     function isValid(bytes32 _platform, string _platformId, address _token, uint256 _value, address _funder) public view returns (bool valid) {
-        return !active || (defaultWhitelist[_token] == true || tokenWhitelist[_platform][_platformId][_token] == true);
+        return !active || (defaultWhitelist[_token] == true || tokenWhitelist[_platform][extractRepository(_platformId)][_token] == true);
     }
 
     function allowDefaultToken(address _token, bool _allowed) public onlyOwner {
@@ -30,5 +34,11 @@ contract TokenWhitelistPrecondition is Precondition {
     function allow(bytes32 _platform, string _platformId, address _token, bool _allowed) public onlyOwner {
         tokenWhitelist[_platform][_platformId][_token] = _allowed;
         Allowed(_token, _allowed, _platform, _platformId);
+    }
+
+    function extractRepository(string _platformId) internal returns (string repository) {
+        var sliced = string(_platformId).toSlice();
+        var platform = sliced.split("|FR|".toSlice());
+        return platform.toString();
     }
 }
