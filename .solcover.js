47cc4820299451a57b561979ae4966f93725340f
Quinten De Swaef <quintendeswaef@gmail.com>
2018-07-03 12:30:05 +0200

    new configuration

--- .solcover.js
+++ .solcover.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports = {
-	skipFiles: [
-		'mocks',
-		'Migrations.sol',
-		'Migrations.sol',
-		'utils/strings.sol',
-		'token/MiniMeToken.sol',
-		'factory/MiniMeTokenFactory.sol',
-	]
+  norpc: true,
+  compileCommand: '../node_modules/.bin/truffle compile',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+  skipFiles: [
+    'mocks',
+    'Migrations.sol',
+    'Migrations.sol',
+    'utils/strings.sol',
+    'token/MiniMeToken.sol',
+    'factory/MiniMeTokenFactory.sol',
+  ],
+  copyNodeModules: true,
 };
